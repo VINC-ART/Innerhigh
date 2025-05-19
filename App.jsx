9c9f189b31a41919ad41641c81dbea610aefd43a
VINC-ART <chaudharivinod15@gmail.com>
2025-05-19 18:53:06 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem' }}>
+      <h1>Welcome to INNERHIGH</h1>
+      <p>Your personalized growth toolkit is live!</p>
+    </div>
+  );
+}
